1d902db0d65eedd86117bde03a21efadf7799688
Fredrick Brennan <copypaste@kittens.ph>
2021-12-22 16:42:00 -0500

    Better error reporting

--- src/notifythread.rs
+++ src/notifythread.rs
@@ -5,26 +5,37 @@ use std::path;
 use std::sync::mpsc::{channel, Sender};
 use std::thread;
 
+#[rustfmt::skip]
 fn launch_impl(dir: path::PathBuf, tx_parent: Sender<path::PathBuf>) {
     let (tx, rx) = channel();
 
     let mut watcher = notify::raw_watcher(tx).unwrap();
-    watcher
-        .watch(&dir, notify::RecursiveMode::Recursive)
-        .unwrap();
-    log::trace!(
-        "Launched notify::RawWatcher in recursive mode on {:?}",
-        &dir
-    );
+    match watcher.watch(&dir, notify::RecursiveMode::Recursive) {
+        Ok(()) => (),
+        Err(notify::Error::Io(e)) => {
+            // log::error! differentiates between static str and literal str
+            macro_rules! LAUNCHFAILMSG {
+                () => ("Cannot launch filesystem watch thread! {}. Won't receive important events!")
+            }
+            if e.kind() == std::io::ErrorKind::PermissionDenied {
+                log::error!(LAUNCHFAILMSG!(), format!("No permissions on target {:?}", &dir));
+            } else {
+                log::error!(LAUNCHFAILMSG!(), format!("I/O error: {:?}", e));
+            }
+            return;
+        }
+        Err(e) => {
+            log::error!("Cannot launch filesystem watch thread! Unknown error: {:?}", e);
+            return;
+        }
+    }
+
+    log::trace!("Launched notify::RawWatcher in recursive mode on {:?}", &dir);
 
     loop {
         let recv = rx.recv();
         match recv {
-            Ok(RawEvent {
-                path,
-                op: Ok(fsop::CLOSE_WRITE),
-                ..
-            }) => {
+            Ok(RawEvent { path, op: Ok(fsop::CLOSE_WRITE), .. }) => {
                 if let Some(path) = path {
                     log::info!("Filesystem write event: {:?}", path);
                     tx_parent.send(path).unwrap();
@@ -36,11 +47,15 @@ fn launch_impl(dir: path::PathBuf, tx_parent: Sender<path::PathBuf>) {
                 log::debug!("Filesystem event: {:?}", recv)
             }
             Ok(event) => log::error!("Broken filesystem event: {:?}", event),
-            Err(e) => log::error!("Filesystem watcher error: {:?}", e),
+            Err(e) => {
+                log::error!("Filesystem watcher error: {:?}. Dying…", e);
+                break;
+            }
         }
     }
 }
 
 pub fn launch(dir: path::PathBuf, tx: Sender<path::PathBuf>) {
+    log::trace!("Spawning fsnotify thread on {:?}; tx {:?}", &dir, &tx);
     thread::spawn(|| launch_impl(dir, tx));
 }
--- src/util.rs
+++ src/util.rs
@@ -1,34 +1,42 @@
+use log;
+
+use std::fmt::Debug;
 use std::path::{Path, PathBuf};
 
 pub trait InUfo<P: AsRef<Path>> {
     fn ufo(&self) -> Option<PathBuf>;
 }
 
-impl<P: AsRef<Path>> InUfo<P> for P {
+impl<P: AsRef<Path> + Debug> InUfo<P> for P {
     fn ufo(&self) -> Option<PathBuf> {
-        fn is_ufo<P: AsRef<Path>>(p: P) -> bool {
+        fn is_ufo<P: AsRef<Path> + Debug>(p: P) -> bool {
             let ufo = p
                 .as_ref()
                 .file_name()
-                .map(|f|f.to_string_lossy().to_lowercase())
-                .unwrap_or_else(||"".into());
-            ufo.ends_with(".ufo") || ufo.ends_with(".ufo3")
+                // handles .UFO / .uFo3
+                .map(|f| f.to_string_lossy().to_lowercase())
+                .unwrap_or_else(|| "".into());
+            let res = ufo.ends_with(".ufo") || ufo.ends_with(".ufo3");
+            log::trace!("InUfo::is_ufo on {:?}: {:?}", p, res);
+            res
         }
-        fn is_glyphs<P: AsRef<Path>>(p: P) -> bool {
-            ["glyphs", "glyphs."].iter().any(|g| {
+        fn is_glyphs<P: AsRef<Path> + Debug>(p: P) -> bool {
+            let res = ["glyphs", "glyphs."].iter().any(|g| {
                 p.as_ref()
                     .file_name()
-                    .map(|f|f.to_str().unwrap())
-                    .unwrap_or_else(||"".into())
+                    .map(|f| f.to_str().unwrap())
+                    .unwrap_or_else(|| "".into())
                     .starts_with(g)
-            })
+            });
+            log::trace!("InUfo::is_glyphs on {:?}: {:?}", p, res);
+            res
         }
         if is_ufo(self) {
-            return Some(self.as_ref().to_path_buf())
+            return Some(self.as_ref().to_path_buf());
         }
         let parent = match self.as_ref().parent() {
             Some(p) => p,
-            None => { return None },
+            None => return None,
         };
         if is_ufo(parent) {
             Some(parent.to_path_buf())
